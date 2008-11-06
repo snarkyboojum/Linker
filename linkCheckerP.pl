1206ffde16c507c75ecd2fc427f0862f8785635c
adrian <snarkyboojum@gmail.com>
2008-11-06 19:16:06 +0000

    Updated license

--- linkCheckerP.pl
+++ linkCheckerP.pl
@@ -1,17 +1,5 @@
 # Copyright 2008 Adrian White
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use warnings;
@@ -148,6 +136,7 @@ sub main {
     storeLink($urlRoot, '');
 
     while ( scalar @{getLinksToVisit()} ) {
+
         my $numLinks = scalar @{getLinksToVisit()};
         my @pids     = ();
         print("Links to visit: [$numLinks]\n");
@@ -155,7 +144,7 @@ sub main {
         my $processLimit = $MAX_PROCESSES; 
 
         if ($numLinks < $MAX_PROCESSES) {
-            $processLimit = scalar @{getLinksToVisit()};
+            $processLimit = $numLinks;
         }
 
         for (my $i = 0; $i < $processLimit; $i++) {
@@ -224,6 +213,7 @@ sub getLinks {
         $parser->parse($content);
     }
     }
+    
     return @links;
 }
 
